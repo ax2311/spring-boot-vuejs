5f0b4a512df005d3beae9457abaecca2acff784a
ax2311 <ax2311@gmail.com>
2018-11-04 18:07:04 +0800

    201811 amend description and pom

--- backend/src/main/java/com/springBlog/Utils.java
+++ backend/src/main/java/com/springBlog/Utils.java
@@ -3,7 +3,7 @@ package com.springBlog;
 public class Utils {
 
 	public static boolean isBlankOrEmpty(String string){
-		if(string == null || "".equals(string) || "null".equals(string))
+		if(string == null || "".equals(string) )
 			return true;
 		else 
 			return false; 
